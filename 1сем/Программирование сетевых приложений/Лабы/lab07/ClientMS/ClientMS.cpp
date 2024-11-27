241069bf4c66e107dd28fc2bde7d9c636b40578a
TheXax <119885135+TheXax@users.noreply.github.com>
2024-11-27 12:44:52 +0300

    Add files via upload

---
+++ 1сем/Программирование сетевых приложений/Лабы/lab07/ClientMS/ClientMS.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <string>
+#include "ClientMS.h"
+
+using namespace std;
+
+string GetErrorMail(int code)
+{
+	string msgText = "";
+	switch (code)
+	{
+	case WSAEINTR: msgText = "Операция была прервана"; break;
+	case WSAEACCES:	msgText = "Доступ запрещен"; break;
+	case WSAEFAULT:	msgText = "Ошибочный адрес"; break;
+	default: msgText = "Error";
+	};
+	return msgText;
+}
+
+string SetErrorMail(string msgText, int code)
+{
+	return msgText + GetErrorMail(code);
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	try {
+		HANDLE clientMailSlot; //дескриптор почтового слота
+		double t1, t2;
+		LPCTSTR name = TEXT("\\\\.\\mailslot\\Box");
+		LPCTSTR NAME[] = {
+			TEXT("\\\\DESKTOP-HPV9KK7\\mailslot\\Box"),
+			TEXT("\\\\Karasik\\mailslot\\Box"),
+			TEXT("\\\\.\\mailslot\\Box")
+		};
+		 //открытие почтового слота для записи
+		//for (int i = 0; i < 3; i++) {
+			if ((clientMailSlot = CreateFile(name, //NAME[i]
+				GENERIC_WRITE,
+				FILE_SHARE_READ | FILE_SHARE_WRITE, //позволяет другим процессам читать из и записывать в почтовый слот, пока он открыт
+				NULL,
+				OPEN_EXISTING, //указывает, что хотим открыть существующий почтовый слот
+				NULL,
+				NULL)) == INVALID_HANDLE_VALUE)
+				throw SetErrorMail("CreateFile: ", GetLastError());
+		
+		cout << "Hello I'm Client" << endl;
+
+		char writeBuf[50] = "Hello from Mailslot-client";
+		DWORD writeMsg; //хранение кол-ва байт, фактически записанных в почтовый слот
+
+		t1 = clock();
+
+		for (int i = 1; i <= 1000; i++) {
+			if(!WriteFile(clientMailSlot,writeBuf,sizeof(writeBuf),&writeMsg,NULL))
+				throw SetErrorMail("WriteFile: ", GetLastError());
+
+			cout << "Message " << i << " was sent" << endl;
+		}
+
+		t2 = clock();
+
+		if (!CloseHandle(clientMailSlot))
+			throw "Error: CloseHandle";
+		//}
+		cout << endl << "Время передачи: " << (t2 - t1) / 1000 << " сек." << endl << endl;
+		system("pause");
+	}
+	catch (string e) {
+		cout << e << endl;
+	}
+	
+	return 0;
+}
---
+++ 1сем/Программирование сетевых приложений/Лабы/lab07/ServerMS/ServerMS.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <string>
+
+using namespace std;
+
+string GetErrorMail(int code)
+{
+	string msgText = "";
+	switch (code)
+	{
+	case WSAEINTR: msgText = "Операция была прервана"; break;
+	case WSAEACCES:	msgText = "Доступ запрещен"; break; //попытка выполнить операцию, к которой нет разрешения
+	case WSAEFAULT:	msgText = "Ошибочный адрес"; break;
+	default: msgText = "Error";
+	};
+	return msgText;
+}
+
+string SetErrorMail(string msgText, int code)
+{
+	return msgText + GetErrorMail(code);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	try {
+		HANDLE serverMailSlot; //дескриптор почтового слота
+		int i = 0;
+		double t1, t2;
+
+		//создание почтового слота
+		if ((serverMailSlot = CreateMailslot(L"\\\\.\\mailslot\\Box",
+			300, //байты
+			MAILSLOT_WAIT_FOREVER,//функция будет ожидать неограниченное время, пока не будет доступно сообщение
+			NULL)) == INVALID_HANDLE_VALUE)
+			throw SetErrorMail("CreateMailslot", GetLastError());
+
+		cout << "Hello I'm server" << endl;
+
+		char readBuf[50]; //буфер прочитанных смс
+		DWORD readMsg; //хранение прочитанных байт
+		do {
+			i++;
+			//чтение из почтового слота
+			if (!ReadFile(serverMailSlot, readBuf, sizeof(readBuf), &readMsg, NULL))
+				throw SetErrorMail("ReadFile", GetLastError());
+			SetMailslotInfo(serverMailSlot, 5000);//таймаут для почтового слота (если ожидает больше 5с, то ошибка)
+			if (i == 1) //если смс первое, то сохраняем время в t1
+				t1 = clock();
+			cout << readBuf << " " << i << endl;
+			
+		} while (strcmp(readBuf, " ") != 0); //пока смс не равно пробелу
+
+		t2 = clock();
+
+		cout << "Время передачи: " << (t2 - t1) / 1000 << " сек." << endl;
+		cout << "Количество сообщений: " << i - 1 << endl << endl;// -1, потому что последнее пробел
+
+		if (!CloseHandle(serverMailSlot))
+			throw "Error: CloseHandle";
+
+		system("pause");
+	}
+	catch (string e) { 
+		if (e == "ReadFileError") {
+			cout << e << ": Timeout";
+			return 0;
+		}
+		cout << e << endl;
+	}
+	return 0;
+}

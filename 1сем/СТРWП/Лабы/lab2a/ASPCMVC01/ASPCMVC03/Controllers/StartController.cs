06c713b22bdc814958e52a2b15afc8c53f38f8d3
TheXax <119885135+TheXax@users.noreply.github.com>
2024-12-20 01:16:16 +0300

    Add files via upload

---
+++ 1сем/СТРWП/Лабы/lab2a/ASPCMVC01/ASPCMVC03/Controllers/StartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCMVC03.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult One()
+        {
+            return Content("<h1>Start/One</h1>", "text/html");
+        }
+
+        public IActionResult Two()
+        {
+            return Content("<h1>Start/Two</h1>", "text/html");
+        }
+
+        public IActionResult Three()
+        {
+            return Content("<h1>Start/Three</h1>", "text/html");
+        }
+    }
+}
---
+++ 1сем/СТРWП/Лабы/lab2a/ASPCMVC01/ASPCMVC04/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCMVC04.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult S200()
+        {
+            int status = new Random().Next(200, 299);
+            return StatusCode(status);
+        }
+
+        public IActionResult S300()
+        {
+            int status = new Random().Next(300, 399);
+            return StatusCode(status);
+        }
+
+        public IActionResult S500()
+        {
+            try
+            {
+                int x = 0;
+                int result = 10 / x;
+            }
+            catch (DivideByZeroException)
+            {
+                return StatusCode(500, "Internal Server Error (500)");
+            }
+
+            return Ok();
+        }
+    }
+}

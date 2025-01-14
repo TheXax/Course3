820da90f4020517360c9c02324e474797c94b522
TheXax <119885135+TheXax@users.noreply.github.com>
2025-01-14 13:15:49 +0300

    Add files via upload

---
+++ 1сем/Компьютерные мультимедийные системы/Лабы/unity/Project/Assets/SwitchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    public Animator animator; // Ссылка на Animator
+    private bool isOn = false; // Состояние выключателя
+
+    // Метод, который вызывается при нажатии на выключатель
+    public void ToggleSwitch()
+    {
+        isOn = !isOn; // Переключаем состояние
+        animator.SetBool("SimulatorOn", isOn); // Устанавливаем значение переменной "turn"
+    }
+
+    // Метод для обработки нажатия на клавиши (можно использовать для тестирования)
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // Нажмите пробел для переключения
+        {
+            ToggleSwitch();
+        }
+    }
+}
---
+++ 1сем/Компьютерные мультимедийные системы/Лабы/unity/Project/Assets/TableController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TableController : MonoBehaviour
+{
+    public BarabanController barabanController;
+
+    public InputField nmInputField; //значение, введённое пользователем
+
+    public float gamma = 2.83e-5f; // γ = 2.83 · 10^-5
+    public float m = 1f; // Примерное значение m
+
+    public int currentIndex = 0; //текущее значение в списке результатов
+
+    private List<CalculationResult> results = new List<CalculationResult>(); //сохранение всех расчётов
+    public List<Row> resultsText = new List<Row>();
+
+    public void CalculateAndSave()
+    {
+        float nm;
+        if (float.TryParse(nmInputField.text, out nm))
+        {
+            float lightLength = barabanController.CurrentLightLength; //получение значения цвета от барабана
+
+            // Вычисляем Dm и Dm2
+            float Dm = nm * gamma;
+            float Dm2 = 4 * m * lightLength * lightLength;
+
+            //сохраняем результаты
+            CalculationResult result = new CalculationResult(nm, Dm, Dm2, lightLength);
+            results.Add(result);
+
+            //заполнение таблицы Canvas
+            resultsText[currentIndex].Number.text = (currentIndex + 1).ToString();
+            resultsText[currentIndex].Nm.text = nm.ToString();
+            resultsText[currentIndex].Dm.text = Dm.ToString();
+            resultsText[currentIndex].Dm2.text = Dm2.ToString();
+
+            if (currentIndex > 0)
+            {
+                CalculationResult prevResult = results[currentIndex - 1]; //предыдущий результат
+                float R = (((Mathf.Abs(D2(Dm2) - D2(prevResult.Dm2)))) / (4 * (nm - prevResult.Nm) * lightLength));
+                Debug.Log(nm + " " + prevResult.Nm + " " + lightLength);
+
+                resultsText[currentIndex].R.text = R.ToString();
+            }
+            else
+            {
+                resultsText[currentIndex].R.text = "-";
+            }
+
+            currentIndex++;
+        }
+        else
+        {
+            Debug.LogError("Некорректный ввод Nm. Убедитесь, что вы ввели число.");
+        }
+    }
+
+    public void ClearResults()
+    {
+        results.Clear();
+        currentIndex = 0;
+
+        foreach (var row in resultsText)
+        {
+            row.Number.text = "";
+            row.Nm.text = "";
+            row.Dm.text = "";
+            row.Dm2.text = "";
+            row.R.text = "";
+        }
+    }
+
+    private float D2(float value)
+    {
+        return value * value;
+    }
+}

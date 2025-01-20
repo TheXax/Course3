5749db056a2852804387bf49647488a5ab2a8e80
TheXax <119885135+TheXax@users.noreply.github.com>
2025-01-21 01:21:33 +0300

    Add files via upload

---
+++ 1сем/СТРWП/Лабы/7/uwsr3/Utils/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace UWSR.Utils
+{
+
+
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value)); //сохраняется в сессии под указанным ключом key
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonSerializer.Deserialize<T>(value);
+        }
+    }
+
+}

ba501d56d336e4cb3a2d87be545ff1fcf2ef5ca3
TheXax <119885135+TheXax@users.noreply.github.com>
2025-03-17 21:08:52 +0300

    Add files via upload

---
+++ 2сем/Криптографические методы защиты информации/Лаба2/lab2/lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using lab2;
+
+var kazakh = Entropy.ReadFromFile("kazakh.txt");
+var spanish = Entropy.ReadFromFile("spanish.txt");
+var binary = Entropy.ReadFromFile("binary.txt");
+var myName = Entropy.ReadFromFile("myName.txt");
+var myNameASCII = Entropy.ReadFromFile("myNameASCII.txt");
+
+
+Console.WriteLine("\n==========================================");
+Console.WriteLine($"Entropy of Language (kazakh):      {Entropy.GetShannonEntropy(kazakh)}");
+Console.WriteLine($"Entropy of Language (spanish):  {Entropy.GetShannonEntropy(spanish)}");
+Console.WriteLine($"Entropy of Language (binary):      {Entropy.GetShannonEntropy(binary)}");
+
+Console.WriteLine("\n================  P = 0  =================");
+Console.WriteLine($"Information Amount (kazakh):       {Entropy.GetInformationAmount(kazakh, myName)}");
+Console.WriteLine($"Information Amount (spanish):   {Entropy.GetInformationAmount(spanish, myName)}");
+Console.WriteLine($"Information Amount (ASCII):        {Entropy.GetInformationAmount(myNameASCII, myNameASCII)}");
+
+Console.WriteLine("\n===============  P = 0.1  ================");
+Console.WriteLine($"Information Amount (kazakh):       {Entropy.GetInformationAmount(kazakh, myName, 0.1)}");
+Console.WriteLine($"Information Amount (spanish):   {Entropy.GetInformationAmount(spanish, myName, 0.1)}");
+Console.WriteLine($"Information Amount (ASCII):        {Entropy.GetInformationAmount(myNameASCII, myNameASCII, 0.1)}");
+
+Console.WriteLine("\n===============  P = 0.5  ================");
+Console.WriteLine($"Information Amount (kazakh):       {Entropy.GetInformationAmount(kazakh, myName, 0.5)}");
+Console.WriteLine($"Information Amount (spanish):   {Entropy.GetInformationAmount(spanish, myName, 0.5)}");
+Console.WriteLine($"Information Amount (ASCII):        {Entropy.GetInformationAmount(myNameASCII, myNameASCII, 0.5)}");
+
+Console.WriteLine("\n================  P = 1  =================");
+Console.WriteLine($"Information Amount (kazakh):       {Entropy.GetInformationAmount(kazakh, myName, 1)}");
+Console.WriteLine($"Information Amount (spanish):   {Entropy.GetInformationAmount(spanish, myName, 1)}");
+Console.WriteLine($"Information Amount (ASCII):        {Entropy.GetInformationAmount(myNameASCII, myNameASCII, 1)}");
